c5ffbd47031a4f8c33dad5d5914b43d11f595d23
Darrell Cornelius R <darrellcornelius37@gmail.com>
2025-03-21 10:29:02 +0700

    update rank definition ddp

--- ddp.py
+++ ddp.py
@@ -7,10 +7,12 @@ from torchvision import models
 
 def setup():
     dist.init_process_group(backend="nccl")
-    rank = dist.get_rank()
+    global_rank = dist.get_rank()
+    local_rank = global_rank % torch.cuda.device_count()
+    
     world_size = dist.get_world_size()
 
-    return rank, world_size
+    return local_rank, global_rank, world_size
 
 
 def cleanup():
@@ -18,19 +20,19 @@ def cleanup():
 
 
 def train():
-    rank, world_size = setup()
+    local_rank, global_rank, world_size = setup()
 
     # Load a predefined ResNet model
     model = models.resnet50(pretrained=True)
-    model = model.to(rank)
-    model = nn.parallel.DistributedDataParallel(model, device_ids=[rank])
+    model = model.to(local_rank)
+    model = nn.parallel.DistributedDataParallel(model, device_ids=[local_rank])
 
     optimizer = optim.Adam(model.parameters(), lr=0.001)
     criterion = nn.CrossEntropyLoss()
 
     # Example dummy data
-    inputs = torch.randn(32, 3, 224, 224).to(rank)
-    labels = torch.randint(0, 1000, (32,)).to(rank)
+    inputs = torch.randn(32, 3, 224, 224).to(local_rank)
+    labels = torch.randint(0, 1000, (32,)).to(local_rank)
 
     # Forward & Backward Pass
     optimizer.zero_grad()
