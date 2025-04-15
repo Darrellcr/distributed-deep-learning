9ed207673af42e6a9bb771c2c7a5d2cb34445dc2
Darrell Cornelius R <darrellcornelius37@gmail.com>
2025-04-15 20:53:10 +0700

    ddp pp

--- ddp_n_pp.py
+++ ddp_n_pp.py
@@ -4,10 +4,10 @@ import random
 import numpy as np
 import pandas as pd
 import torch
-from torch import optim
-import torch.distributed as dist
+from torch import optim, nn
+import torch.distributed as distf
 from torch.distributed.device_mesh import init_device_mesh, DeviceMesh
-from torch.distributed.pipelining import pipeline, SplitPoint, ScheduleGPipe
+from torch.distributed.pipelining import SplitPoint, ScheduleGPipe, PipelineStage
 from torch.nn import functional as F
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.data import Dataset, DataLoader, DistributedSampler
@@ -72,9 +72,9 @@ class AptosDataset(Dataset):
 class Trainer:
     def __init__(
         self,
-        model: torch.nn.Module,
+        model_stages: list[torch.nn.Module],
         train_data: DataLoader,
-        optimizer: torch.optim.Optimizer,
+        optimizers: list[torch.optim.Optimizer],
         device_mesh: DeviceMesh,
         num_microbatches: int = 4,
         save_every: int = 0,
@@ -83,9 +83,10 @@ class Trainer:
         self.global_rank = dist.get_rank()
         self.local_rank = self.global_rank % torch.cuda.device_count()
         self.device = torch.device(f'cuda:{self.local_rank}')
-        self.model = model.to(self.local_rank)
+        self.model_stages = model_stages
         self.train_data: DataLoader[torch.Tensor] = train_data
-        self.optimizer = optimizer
+        self.optimizers = optimizers
+        self.device_mesh = device_mesh
         self.save_every = save_every
         self.epochs_run = 0
         self.snapshot_path = snapshot_path
@@ -96,17 +97,25 @@ class Trainer:
 
         first_batch: list[torch.Tensor] = next(iter(self.train_data))
         x, _ = first_batch
-        x_mb = x.chunk(self.num_microbatches)
+        x_mbs = x.chunk(self.num_microbatches)
 
-        self.model.to(self.device)
-        self.model_pipeline = pipeline(
-            self.model,
-            mb_args=(x_mb),
+        self.model_stage = self.model_stages[self.local_rank]
+        self.model_stage.to(self.device)
+        self.model_stage = DDP(self.model_stage, process_group=self.device_mesh['dp'].get_group())
+        self.model_stage = PipelineStage(
+            self.model_stage,
+            stage_index=self.local_rank,
+            num_stages=len(self.model_stages),
+            device=self.device,
+            group=self.device_mesh['pp'].get_group(),
+            input_args=x_mbs[0],
         )
 
-        print('split model')
-        print(self.model_pipeline)
-
+        self.schedule = ScheduleGPipe(
+            self.model_stage,
+            n_microbatches=self.num_microbatches,
+            loss_fn=F.cross_entropy,
+        )
 
     def _load_snapshot(self, snapshot_path):
         loc = f"cuda:{self.local_rank}"
@@ -114,24 +123,6 @@ class Trainer:
         self.model.load_state_dict(snapshot["MODEL_STATE"])
         self.epochs_run = snapshot["EPOCHS_RUN"]
         print(f"Resuming training from snapshot at Epoch {self.epochs_run}")
-
-    def _run_batch(self, source, targets):
-        self.optimizer.zero_grad()
-        output = self.model(source)
-        loss = F.cross_entropy(output, targets)
-        loss.backward()
-        self.optimizer.step()
-
-    def _run_epoch(self, epoch):
-        b_sz = len(next(iter(self.train_data))[0])
-        self.train_data.sampler.set_epoch(epoch)
-        print(f"[GPU{self.global_rank}] Starting Epoch {epoch}")
-        for source, targets in tqdm(self.train_data):
-            source = source.to(self.local_rank)
-            targets = targets.to(self.local_rank)
-            self._run_batch(source, targets)
-        print(
-            f"[GPU{self.global_rank}] Epoch {epoch} | Batchsize: {b_sz} | Steps: {len(self.train_data)}")
 
     def _save_snapshot(self, epoch):
         snapshot = {
@@ -142,6 +133,34 @@ class Trainer:
         print(
             f"Epoch {epoch} | Training snapshot saved at {self.snapshot_path}")
 
+    def _run_batch(self, source, targets):
+        # self.optimizer.zero_grad()
+        for optimizer in self.optimizers:
+            optimizer.zero_grad()
+
+        if self.local_rank == 0:
+            self.schedule.step(source)
+        else:
+            output= self.schedule.step()
+
+        loss = F.cross_entropy(output, targets)
+        loss.backward()
+        
+        # self.optimizer.step()
+        for optimizer in self.optimizers:
+            optimizer.step()
+
+    def _run_epoch(self, epoch):
+        b_sz = len(next(iter(self.train_data))[0])
+        self.train_data.sampler.set_epoch(epoch)
+        print(f"[GPU{self.global_rank}] Starting Epoch {epoch}")
+        for source, targets in tqdm(self.train_data):
+            source = source.to('cuda:0')
+            targets = targets.to('cuda:1')
+            self._run_batch(source, targets)
+        print(
+            f"[GPU{self.global_rank}] Epoch {epoch} | Batchsize: {b_sz} | Steps: {len(self.train_data)}")
+    
     def train(self, max_epochs: int):
         for epoch in range(self.epochs_run, max_epochs):
             self._run_epoch(epoch)
@@ -164,16 +183,32 @@ def main():
     data_loader = DataLoader(dataset, batch_size=8, sampler=sampler)
     # model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
     model = models.resnet50()
-    optimizer = optim.Adam(model.parameters())
+    stage1 = nn.Sequential([
+        model.conv1,
+        model.bn1,
+        model.relu,
+        model.maxpool,
+        model.layer1,
+        model.layer2,
+    ])
+    stage2 = nn.Sequential([
+        model.layer3,
+        model.layer4,
+        model.avgpool,
+        nn.Flatten(),
+        model.fc,
+    ])
+    model_stages = [stage1, stage2]
+    optimizers = [optim.Adam(stage.parameters()) for stage in model_stages]
 
     trainer = Trainer(
-        model=model,
+        model_stages=model_stages,
         train_data=data_loader,
-        optimizer=optimizer,
+        optimizers=optimizers,
         device_mesh=device_mesh,
         num_microbatches=4,
     )
-    # trainer.train(max_epochs=2)
+    trainer.train(max_epochs=1)
 
     cleanup()
 
