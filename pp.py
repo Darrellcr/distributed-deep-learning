23ae3d1377e9c9ee296c589ed54c36b870a4713e
Darrell Cornelius R <darrellcornelius37@gmail.com>
2025-05-12 19:51:11 +0700

    torch benchmark false

--- pp.py
+++ pp.py
@@ -41,7 +41,7 @@ def set_seed(seed):
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = True
+    torch.backends.cudnn.benchmark = False
 
 
 class Normalize:
