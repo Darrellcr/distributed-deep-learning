c4aa2f5a104a88b24e99350a34f7a8d1a256b843
Darrell Cornelius R <darrellcornelius37@gmail.com>
2025-06-21 09:50:43 +0700

    fix: ddpnpp mesh size

--- ddp_n_pp.py
+++ ddp_n_pp.py
@@ -30,7 +30,7 @@ CHECKPOINT_DIR = "/mnt/dcornelius/checkpoints/ddpnpp"
 def setup():
     dist.init_process_group(backend='nccl')
     device_mesh = init_device_mesh(
-        'cuda', mesh_shape=(3, 2), mesh_dim_names=('dp', 'pp'))
+        'cuda', mesh_shape=(2, 2), mesh_dim_names=('dp', 'pp'))
     local_rank = dist.get_rank() % torch.cuda.device_count()
     torch.cuda.set_device(local_rank)
     
