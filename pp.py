a15722fa11bacb0a1e959b868fac69a75c9b6770
Darrell Cornelius R <darrellcornelius37@gmail.com>
2025-06-30 12:30:20 +0700

    pp add generator

--- pp.py
+++ pp.py
@@ -366,8 +366,9 @@ def main():
     train_loader = DataLoader(
         train_dataset, batch_size=batch_size, drop_last=True, shuffle=True, generator=g, num_workers=2
     )
+    g = torch.Generator()
     test_loader = DataLoader(
-        test_dataset, batch_size=batch_size, drop_last=True, shuffle=True, num_workers=2
+        test_dataset, batch_size=batch_size, drop_last=True, shuffle=True, generator=g, num_workers=2
     )
 
     model = models.densenet121(weights=models.DenseNet121_Weights.IMAGENET1K_V1)
