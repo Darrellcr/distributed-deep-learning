8a23c0f5409ce0e62e52f972923c0657728f6da1
Darrell Cornelius R <darrellcornelius37@gmail.com>
2025-07-02 10:23:43 +0700

    communication time

---
+++ communication_time.py
@@ -0,0 +1,66 @@
+import torch
+import torch.distributed as dist
+import os
+from pathlib import Path
+
+
+def setup():
+    dist.init_process_group(backend='nccl')
+    local_rank = dist.get_rank() % torch.cuda.device_count()
+    torch.cuda.set_device(local_rank)
+
+
+def cleanup():
+    dist.destroy_process_group()
+
+
+def measure_communication_time():
+    tensor_size = 1024 * 1024 
+    global_rank = dist.get_rank()
+    local_rank = global_rank % torch.cuda.device_count()
+
+    if global_rank == 0:
+        tensor = torch.randn(tensor_size, dtype=torch.float32, device=f"cuda:{local_rank}")
+        ack = torch.zeros(1, dtype=torch.float32, device=f"cuda:{local_rank}")
+
+        start_time = torch.cuda.Event(enable_timing=True)
+        end_time = torch.cuda.Event(enable_timing=True)
+
+        start_time.record()
+        dist.send(tensor, dst=1)
+        dist.recv(ack, src=1)
+        end_time.record()
+
+        torch.cuda.synchronize()
+        elapsed_time = start_time.elapsed_time(end_time)
+        print(f"Rank {global_rank} sent tensor of size {tensor_size} in {elapsed_time:.2f} ms")
+        return elapsed_time
+    elif global_rank == 1:
+        tensor = torch.zeros(tensor_size, dtype=torch.float32, device=f"cuda:{local_rank}")
+        ack = torch.ones(1, dtype=torch.float32, device=f"cuda:{local_rank}")
+
+        dist.recv(tensor, src=0)
+        dist.send(ack, dst=0)
+
+        torch.cuda.synchronize()
+
+
+
+def main():
+    setup()
+    job_id = os.getenv("TORCHX_JOB_ID", "local").split("/")[-1]
+
+    # elapsed_time = measure_communication_time()
+    if dist.get_rank() == 0:
+        with open("/mnt/dcornelius/training_logs/communication_time.csv", "a") as f:
+            for i in range(1000):
+                elapsed_time = measure_communication_time()
+                f.write(f"{job_id},{i},{elapsed_time}\n")
+    elif dist.get_rank() == 1:
+        for i in range(1000):
+            measure_communication_time()
+
+    cleanup()
+
+if __name__ == "__main__":
+    main()
